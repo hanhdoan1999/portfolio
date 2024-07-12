4d75be1394f8f50b979f4ec25989a5c769b855cd
HanhDHT <doanhonghanh.1999@gmail.com>
2024-07-12 16:37:17 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import App from './App';
 import "./styles/fonts.scss";
+import './index.scss';
 import DarkModeProvider from './Context/useDarkMode';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
